3e8fdf83224eb8cd8e803e2735c32ab3bb93b0f9
prof-csh <prof.csh@gmail.com>
2019-11-10 15:34:22 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngx-gallery-cg4ehl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngx-gallery-cg4ehl)
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,62 @@
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Gallery, GalleryItem, ImageItem, ThumbnailsPosition, ImageSize } from '@ngx-gallery/core';
+import { Lightbox } from '@ngx-gallery/lightbox';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeComponent implements OnInit {
+
+  items: GalleryItem[];
+
+  imageData = data;
+
+  constructor(public gallery: Gallery, public lightbox: Lightbox) {
+  }
+
+  ngOnInit() {
+
+    /** Basic Gallery Example */
+
+    // Creat gallery items
+    this.items = this.imageData.map(item => new ImageItem({ src: item.srcUrl, thumb: item.previewUrl }));
+
+
+    /** Lightbox Example */
+
+    // Get a lightbox gallery ref
+    const lightboxRef = this.gallery.ref('lightbox');
+
+    // Add custom gallery config to the lightbox (optional)
+    lightboxRef.setConfig({
+      imageSize: ImageSize.Cover,
+      thumbPosition: ThumbnailsPosition.Top
+    });
+
+    // Load items into the lightbox gallery ref
+    lightboxRef.load(this.items);
+  }
+}
+
+const data = [
+  {
+    srcUrl: 'https://preview.ibb.co/jrsA6R/img12.jpg',
+    previewUrl: 'https://preview.ibb.co/jrsA6R/img12.jpg'
+  },
+  {
+    srcUrl: 'https://preview.ibb.co/kPE1D6/clouds.jpg',
+    previewUrl: 'https://preview.ibb.co/kPE1D6/clouds.jpg'
+  },
+  {
+    srcUrl: 'https://preview.ibb.co/mwsA6R/img7.jpg',
+    previewUrl: 'https://preview.ibb.co/mwsA6R/img7.jpg'
+  },
+  {
+    srcUrl: 'https://preview.ibb.co/kZGsLm/img8.jpg',
+    previewUrl: 'https://preview.ibb.co/kZGsLm/img8.jpg'
+  }
+];
